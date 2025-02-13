5fec38b8983f2fd0f330c75bccc11e22040a4a37
ozan <ozan@ozans-MacBook-Air.local>
2025-02-13 12:19:05 +0700

    [MV-Project-1] meta view port

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+
         <Head />
         <body>
           <Main />
